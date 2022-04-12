f8b29863b5b46e8a38f998b5d1a974cd2e537f1f
Andrés Santiago Calderón Peña <santiagocalderon@MacBook-Pro-de-Andres.local>
2022-04-12 11:55:58 -0500

    initial commit

---
+++ src/main/java/com/cryptoai/javaapi/JavaAiCryptoApiApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoai.javaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiCryptoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAiCryptoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cryptoai/javaapi/JavaAiCryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoai.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiCryptoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
